e876ff623e14682d21330f82edf1a2b0e71974f8
Rodrigo Ruas <rodrigoruas2@gmail.com>
2017-02-06 15:42:12 -0200

    ok..

--- config.rb
+++ config.rb
@@ -89,3 +89,11 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
+
+data.cards.each do |flat|
+  proxy "/flats/#{flat.owner}.html", "show.html", :locals => { :card => flat }, :ignore => true
+
+end
+
+
